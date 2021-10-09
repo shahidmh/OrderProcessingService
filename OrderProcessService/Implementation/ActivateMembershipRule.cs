5c8111abd47ae520f6053f41e3582e6837b6e993
shahidmh <shahidmhidayath@gmail.com>
2021-10-09 14:39:28 +0530

    membership upgrade rule added

---
+++ OrderProcessService/Implementation/ActivateMembershipRule.cs
@@ -0,0 +1,14 @@
+﻿using OrderProcessService.Interface;
+
+namespace OrderProcessService.Implementation
+{
+    public class ActivateMembershipRule : IOrderProcessRule
+    {
+        public bool IsRuleApplied { get; set; }
+
+        public string ApplyOrderRule()
+        {
+            return "activate membership";
+        }
+    }
+}
---
+++ OrderProcessService/Implementation/UpgradeMemberShipRule.cs
@@ -0,0 +1,16 @@
+﻿using OrderProcessService.Interface;
+
+namespace OrderProcessService.Implementation
+{
+    public class UpgradeMemberShipRule : IOrderProcessRule
+    {
+        public bool IsRuleApplied { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+
+        public string ApplyOrderRule()
+        {
+            return "Upgarde membership";
+        }
+
+
+    }
+}
