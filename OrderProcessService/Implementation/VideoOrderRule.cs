3c374cb43501592316c47c07e26c96668bf57683
shahidmh <shahidmhidayath@gmail.com>
2021-10-09 16:29:54 +0530

    Video order rule is added

---
+++ OrderProcessService/Implementation/VideoOrderRule.cs
@@ -0,0 +1,18 @@
+﻿using OrderProcessService.Interface;
+using OrderProcessService.Model;
+
+namespace OrderProcessService.Implementation
+{
+    public class VideoOrderRule : IOrderProcessRule
+    {
+        public bool IsApplicable(Order order)
+        {
+            return order.ProductTypes == ProductTypes.Video;
+        }
+
+        public string ProcessRules(Order order)
+        {
+            return "Add a free “First Aid” video to the packing slip (the result of a court decision in 1997).";
+        }
+    }
+}
---
+++ OrderProcessService/Interface/IOrderService.cs
@@ -0,0 +1,13 @@
+﻿using OrderProcessService.Model;
+
+namespace OrderProcessService.Interface
+{
+
+    /// <summary
+    /// inteface for processing order 
+    /// </summary>
+    public interface IOrderService
+    {
+        public string ProcessOrder(Order productType);
+    }
+}
