50c0648fde1e2486460dec0c1bb093f8684ab1c6
shahidmh <shahidmhidayath@gmail.com>
2021-10-09 17:05:40 +0530

    Console app added

---
+++ OrderApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using OrderProcessService;
+using OrderProcessService.Implementation;
+using OrderProcessService.Interface;
+using OrderProcessService.Model;
+using System;
+
+namespace OrderApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var order = new Order()
+            {
+                OrderId = 1232,
+                ProductTypes = ProductTypes.ActivateMemberShip
+            };
+
+            IOrderService orderService = new OrderService();
+            var res = orderService.ProcessOrder(order);
+            Console.WriteLine(res);
+            Console.ReadLine();
+        }
+    }
+}
