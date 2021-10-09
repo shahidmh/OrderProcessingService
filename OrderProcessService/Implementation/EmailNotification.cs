5635a74f93b628f4b63a5f2faec3e4d11196d7b3
shahidmh <shahidmhidayath@gmail.com>
2021-10-09 17:07:06 +0530

    Email notification class added

---
+++ OrderProcessService/Implementation/EmailNotification.cs
@@ -0,0 +1,17 @@
+﻿using OrderProcessService.Model;
+using System;
+
+namespace OrderProcessService.Implementation
+{
+    public class EmailNotification
+    {
+        public void SendEmail(Order order)
+        {
+            if (order.ProductTypes == ProductTypes.ActivateMemberShip)
+                Console.WriteLine($"Your account is activated now");
+            else if (order.ProductTypes == ProductTypes.ActivateMemberShip)
+                Console.WriteLine($"Your account is upgraded now");
+
+        }
+    }
+}
